24f00450c10ccb500dd5f6e6df6081cd23d92da3
Kamil Chmiel <kamilchmiel@windowslive.com>
2019-12-30 11:58:21 +0100

    Set MyProfile view

--- app/containers/Profile/index.tsx
+++ app/containers/Profile/index.tsx
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import { Text, View, Dimensions } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Dimensions} from 'react-native';
+import {Profile} from '../../models';
 
-import { Container } from './styles';
+import {
+  Container,
+  TouchableView,
+  Picture,
+  Name,
+  TreningType,
+  DataContainer,
+  Description,
+} from './styles';
 
 interface Props {}
 
-interface State {}
-
-class Profile extends Component<Props, State> {
-	render() {
-		return (
-			<Container width={Dimensions.get('window').width}>
-				<Text>Profile</Text>
-			</Container>
-		);
-	}
+interface State {
+  profile: Profile;
 }
 
-export default Profile;
+class MyProfile extends Component<Props, State> {
+  state = {
+    profile: null,
+  };
+
+  render() {
+    return (
+      <Container width={Dimensions.get('window').width}>
+        <TouchableView activeOpacity={1}>
+          <Picture source={require('../../assets/male-profile-picture.png')} />
+        </TouchableView>
+        <DataContainer>
+          <Name>Tom</Name>
+          <TreningType>Trening</TreningType>
+          <Description>
+            TestowyTestowyTestowyTestowyTestowyTestowyTestowyTestowy
+          </Description>
+        </DataContainer>
+      </Container>
+    );
+  }
+}
+
+export default MyProfile;
--- app/containers/Profile/styles.ts
+++ app/containers/Profile/styles.ts
@@ -1,4 +1,56 @@
 import styled from 'styled-components/native';
-import { Dimensions } from 'react-native';
+import {Dimensions} from 'react-native';
 
-export const Container = styled.View`width: ${(props) => props.width};`;
+export const Container = styled.View`
+  width: ${props => props.width};
+`;
+
+export const TouchableView = styled.TouchableOpacity`
+  margin-top: 30;
+  width: 80%;
+  align-self: center;
+  justify-content: center;
+`;
+
+export const DataContainer = styled.View`
+  width: 90%;
+  align-self: center;
+`;
+
+export const Picture = styled.Image`
+  width: 100%;
+  padding-top: 100%;
+  background-color: grey;
+  border-radius: 10;
+`;
+
+export const Name = styled.Text`
+  margin-top: 10;
+  margin-left: 5;
+  font-weight: 400;
+  font-size: 22;
+  text-align: center;
+  background-color: lightgrey;
+  border-radius: 5;
+  overflow: hidden;
+`;
+
+export const TreningType = styled.Text`
+  margin-top: 8;
+  margin-left: 5;
+  font-size: 17;
+  text-align: center;
+  background-color: lightgrey;
+  border-radius: 5;
+  overflow: hidden;
+`;
+
+export const Description = styled.Text`
+  margin-top: 8;
+  margin-left: 5;
+  font-size: 14;
+  text-align: center;
+  background-color: lightgrey;
+  border-radius: 5;
+  overflow: hidden;
+`;
